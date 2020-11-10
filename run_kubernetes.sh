61bdb09f4a0e87665ef3485e8256d017630ce8fa
James B. Shumate <59979068+twistyx-dev@users.noreply.github.com>
2020-11-10 00:16:07 -0600

    Update run_kubernetes.sh

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,13 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
-
+dockerpath=twi5tyx/minikube
 # Step 2
 # Run the Docker Hub container with kubernetes
-
-
+kubectl create deployment udacity-project-5 --image=$dockerpath
 # Step 3:
 # List kubernetes pods
-
+kubectl get deployments
 # Step 4:
 # Forward the container port to a host
-
+kubectl proxy
