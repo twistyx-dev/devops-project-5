a3a035dd4c7ca33ad7121334fe634cdc0c2a84dc
James B. Shumate <59979068+twistyx-dev@users.noreply.github.com>
2020-11-28 10:12:31 -0600

    update to running docker

--- run_docker.sh
+++ run_docker.sh
@@ -3,8 +3,8 @@
 ## Complete the following steps to get Docker running locally
 
 # Step 1:
-docker build --tag=project-5 .
+docker build --tag=twi5tyx/project-5 .
 # Step 2:
 docker image ls
 # Step 3:
-docker run -p 8000:80 project-5
+docker run -p 8000:80 twi5tyx/project-5
